cb6ab3069d58f9898f377c7ff430bd28faf76ebc
kennuka <gledses@mail.ru>
2023-09-06 16:11:31 +0300

    add .

---
+++ homework 2.py
@@ -0,0 +1,5 @@
+radius = input('Введите радиус круга: ')
+pi=float(3.14)
+lenght = int(radius) * 2 *pi
+square = ((int(radius))**2)*2*pi
+print('Длина окружности составляет:',str(lenght)[:8] ,'\nПлощадь круга составляет:',str(square)[:8])
